1aee70f7b315f300ba05d15be4e06ef11fce65bd
Chandrabhan <151602980+dudhwal1802@users.noreply.github.com>
2025-12-23 19:59:17 +0530

    Fix [index.html](http://_vscodecontentref_/6) route for Pages

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ const App = () => (
       <BrowserRouter basename={routerBasename}>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/index.html" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
