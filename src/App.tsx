be7c852270609f0fd535f224e88b566ef2476941
Chandrabhan <151602980+dudhwal1802@users.noreply.github.com>
2025-12-27 13:35:20 +0530

    SEO

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ const App = () => (
         <CommandPalette />
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/index.html" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
